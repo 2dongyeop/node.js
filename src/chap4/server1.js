376834be9bc701b9df7cb71ef1c10a2710f2bca0
Leedongyeop <ldy_1204@naver.com>
2022-02-06 14:01:06 +0900

    실습파일 작성

--- src/chap4/server1.js
+++ src/chap4/server1.js
@@ -1,9 +1,9 @@
-const http = require('http');
-//import http from 'http';
+//http = require('http');
+import http from 'http';
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>Hello Node!<h1>');
+    res.write('<h1>Hello Node!</h1>');
     res.end('<p>Hello Server!</p>');
 })
     .listen(8080, () => {  //서버 연결
