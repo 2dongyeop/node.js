6381cbd2b867c2828fb1ef61b04371b8a0dfe857
leedongyeop <ldy_1204@naver.com>
2022-03-20 18:54:47 +0900

    chapter12 code upload

---
+++ src/chap12/12.6gif-chat/routes/index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const rooms = await Room.find({});
+    res.render('main', { rooms, title: 'GIF 채팅방' });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/room', (req, res) => {
+  res.render('room', { title: 'GIF 채팅방 생성' });
+});
+
+router.post('/room', async (req, res, next) => {
+  try {
+    const newRoom = await Room.create({
+      title: req.body.title,
+      max: req.body.max,
+      owner: req.session.color,
+      password: req.body.password,
+    });
+    const io = req.app.get('io');
+    io.of('/room').emit('newRoom', newRoom);
+    res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/room/:id', async (req, res, next) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    const io = req.app.get('io');
+    if (!room) {
+      return res.redirect('/?error=존재하지 않는 방입니다.');
+    }
+    if (room.password && room.password !== req.query.password) {
+      return res.redirect('/?error=비밀번호가 틀렸습니다.');
+    }
+    const { rooms } = io.of('/chat').adapter;
+    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+      return res.redirect('/?error=허용 인원이 초과하였습니다.');
+    }
+    const chats = await Chat.find({ room: room._id }).sort('createdAt');
+    //방 접속 시 기존 채팅 내역을 불러오도록 수정
+    //DB로부터 채팅 내역을 가져오고, 접속 후에는 웹 소켓으로 새로운 채팅 메시지를 받음
+    return res.render('chat', {
+      room,
+      title: room.title,
+      chats,
+      user: req.session.color,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+router.delete('/room/:id', async (req, res, next) => {
+  try {
+    await Room.remove({ _id: req.params.id });
+    await Chat.remove({ room: req.params.id });
+    res.send('ok');
+    setTimeout(() => {
+      req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/room/:id/chat', async (req, res, next) => {
+  try { //채팅을 할 때마다 채팅 내용이 이 라우터로 전송되고, 라우터에서 다시 웹 소켓으로 메시지를 전송
+    const chat = await Chat.create({
+      room: req.params.id,
+      user: req.session.color,
+      chat: req.body.chat,
+    });
+    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+    //채팅을 데이터베이스에 저장한 후 같은 방에 들어 있는 소켓들에게 메시지 데이터를 전송
+    res.send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
