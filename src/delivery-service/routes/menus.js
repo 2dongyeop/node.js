f20a23ca479abf2b7265db5d947791b7e717e30f
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-04-14 21:48:40 +0900

    13.2장을 활용한 로그인~음식 등록~주문 기능 구현

--- src/delivery-service/routes/menus.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET menus listing. */
-router.get('/', function(req, res, next) {
-  res.send('menu page');
-});
-
-module.exports = router;
