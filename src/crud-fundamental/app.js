7f97b258de694ee9218bfbcabd107b8dab97584d
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-13 14:08:21 +0900

    코드 일부 수정

--- src/crud-fundamental/app.js
+++ src/crud-fundamental/app.js
@@ -13,9 +13,13 @@ const menu = `<!DOCTYPE html>
 <body>
     <h1> 진욱이네 중국집 메뉴판<h1>
     <h2> <주의사항> 곱빼기는 주문 안됩니다. <h2>
+    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA0MTJfMjU1%2FMDAxNjQ5NzUyMDMxODE5.PFdMpzVbDxpt0piDk04m2VyhU__5utAvwyKbm6fBEbwg.6iGqePo8vTCifT8A4ftznH-qk-Nilkk5H34bX2azw0Eg.JPEG.hidada2022%2FIMG_0803.jpg&type=a340" width="200" height="200">
     <h3> 짜장면 - 7,000원 <h3>
+    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA0MTJfNCAg%2FMDAxNjQ5NzQ4MTAwMjU4.D_fr_vETO_8op5TdCb7F87hLr9G1WIQSsiE3RT0g3Fwg.COZVRLpr-tBpa1_rrMEmG3hGV3ENpFWiDdYemSYeHJMg.JPEG.kyyee6000%2F20220412%25A3%25DF123420.jpg&type=sc960_832" width="200" height="200">
     <h3> 짬뽕 - 8,000원 <h3>
+    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA0MTFfMTYg%2FMDAxNjQ5Njc1NjI4MTEz.D0zwiwYU46UzqyuyFvM-IKI4ZHLCMgCrlYtXxk51uikg.ojOy70TsrxKeG0OhfDqojGx7DmigGA-0aROsMnnE_KUg.JPEG.sjlllee%2FKakaoTalk_20220409_125823632_14.jpg&type=a340" width="200" height="200">    
     <h3> 탕수육 - 16,000원 <h3>
+    <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA1MDFfMTE2%2FMDAxNjUxMzY2NTI5Nzk1.EfMHF2mdfELVCuMaPF228ok7iV0dChZwz0rDH6F76Uog.BtWTnDvgfs71Ytbl5gjeOZ2ucr2FnrfKLON1DNbvefIg.JPEG.onelovemy%2F20220429%25A3%25DF131628.jpg&type=sc960_832" width="200" height="200">
     <h3> 팔보채 - 59,000원 <h3>
 </body>
 `
@@ -32,7 +36,7 @@ const server = http.createServer((req, res) => {
         console.log();
         fs.readdir(`./data`, 'utf8', (err, files) => {
             if (err) throw err;
-            //테이블(파일) 별 주문내역(내용) 출력
+            //테이블(파일) 별 주문내역(내용)을 불러옴
             fs.readFile(`./data/1st-table.txt`, 'utf8', (err2, data) => {
                 if (err2) throw err2;
                 res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -71,14 +75,14 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end("UPDATE ORDER LIST");
         });
-        console.log("서비스 음료 적립, 각 테이블 별 주문 내역이 수정되었습니다.")
+        console.log("UPDATE ORDER LIST")
 
     } else if (pathName === '/delete') {
         console.log();
         //Delete
         //추가 주문 파일이 삭제되도록 지정
         console.log("일일 정산을 위해 서비스 내역 삭제")
-        fs.unlink(`./data/addtional-order.txt`, () => {
+        fs.unlink(`./data/additional-order.txt`, () => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end("DELETE ORDER LIST");
         });
